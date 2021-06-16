b0ed0a3dadb4885f4186b671d0e3a9ce96f90b75
newketom <liuke0821@163.com>
2021-06-16 23:50:57 +0800

    feat: login

---
+++ src/api/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import mpAdapter from "axios-miniprogram-adapter";
+import Taro from '@tarojs/taro';
+import { CategoryClient, AuthClient, ConsigneeClient, AccountClient, ShoppingCartClient, OrderClient, RiderClient } from "./client";
+
+// @ts-ignore
+axios.defaults.adapter = mpAdapter;
+axios.interceptors.request.use(
+    (config) => {
+        config.withCredentials = true; // 允许携带token ,这个是解决跨域产生的相关问题
+        config.timeout = 6000;
+        config.headers = {
+            "Authorization": "Bearer " + Taro.getStorageSync('token'),
+        };
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+const apiurl = "https://api-dev-daveshop-wechat.chinacloudsites.cn";
+const apiurl1 = "http://localhost:4888";
+export const API = {
+    commodityClient: new CategoryClient(apiurl, axios),
+    authClient: new AuthClient(apiurl, axios),
+    consigneeClient: new ConsigneeClient(apiurl, axios),
+    accountClient: new AccountClient(apiurl, axios),
+    orderClient: new OrderClient(apiurl, axios),
+    categoryClient: new CategoryClient(apiurl, axios),
+    shoppingCartClient: new ShoppingCartClient(apiurl, axios),
+    riderClient: new RiderClient(apiurl, axios),
+};

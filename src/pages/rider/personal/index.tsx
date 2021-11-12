0b21fe5b1b0878d780a4a538470767ca3824e3a9
Liu Ke <liuke0821@163.com>
2021-11-12 09:58:37 +0800

    feat: rider service

--- src/pages/rider/personal/index.tsx
+++ src/pages/rider/personal/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from "@tarojs/components";
+import { Image, Navigator, View } from "@tarojs/components";
 import React from "react";
 import { useSelector } from "react-redux";
 import { RiderGetSummaryResponse } from "src/api/client";
@@ -7,11 +7,28 @@ import { UserModelState } from "src/models/user";
 import './index.scss'
 import defaultUserAvator from '/src/static/images/my/user.png'
 import lv1Svg from '/src/static/images/my/rider/level-1.svg'
+import riderSvg from '/src/static/images/my/rider/rider.svg'
+import settlementSvg from '/src/static/images/my/rider/settlement.svg'
 import NumberMotion from "./components/NumberMotion";
+import { AtIcon } from "taro-ui";
 
 export type PersonalProps = {
     summary: RiderGetSummaryResponse
 };
+
+const renderListItem = (title: string, path: string, desc: string, showArrow: boolean) => {
+    return (<Navigator url={path} style={{ padding: '20px 20px' }}>
+        <View style={{ display: 'flex', alignItems: 'center' }}>
+            <View style={{ fontSize: 16, color: '#343434' }}>{title}</View>
+            <View style={{ flex: 1, textAlign: 'right', fontSize: 14, color: '#C1C3C6' }}>{desc}</View>
+            <View>
+                {showArrow &&
+                    <AtIcon value='chevron-right' size={14} color='#C1C3C6' customStyle={{ marginLeft: 10, verticalAlign: 'unset' }}></AtIcon>
+                }
+            </View>
+        </View>
+    </Navigator>)
+}
 const Personal: React.FC<PersonalProps> = (props) => {
     const { currentUser, isLogin } = useSelector<ConnectState, UserModelState>(x => x.user)
     return (
@@ -41,6 +58,23 @@ const Personal: React.FC<PersonalProps> = (props) => {
                     <View> <NumberMotion number={987} /> </View>
                     <View>  累计配送 </View>
                 </View>
+            </View>
+            <View className="service-card">
+                <View className="title">骑士服务</View>
+                <View className="services">
+                    <View>
+                        <Image src={settlementSvg} ></Image>
+                        <View>  结算记录 </View>
+                    </View>
+                    <View>
+                        <Image src={riderSvg} ></Image>
+                        <View>  配送历史 </View>
+                    </View>
+                </View>
+                {/* {renderListItem('结算记录', '/pages/rider/index', '积分换购', true)}
+                {renderListItem('配送历史', '', '7*24在线客服', true)}
+                {renderListItem('技术支持', '', 'LIMO科技', true)}
+                {renderListItem('关于我们', '', ' ', true)} */}
             </View>
         </View>
     )

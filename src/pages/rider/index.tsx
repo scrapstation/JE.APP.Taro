0946898b0a06b7d07a0ca109e1f8388d2c3bdf22
ok <liuke0821@163.com>
2021-11-16 23:52:15 +0800

    feat: personal

--- src/pages/rider/index.tsx
+++ src/pages/rider/index.tsx
@@ -1,92 +1,88 @@
-import React, { useEffect, useState } from "react";
-import { API } from "../../../src/api";
-import { AtMessage, AtTabBar } from "taro-ui";
-import Deliveries from "./deliveries";
-import Personal from "./personal";
+import React, { useEffect, useState } from 'react';
+import { API } from '../../../src/api';
+import { AtMessage, AtTabBar } from 'taro-ui';
+import Deliveries from './deliveries';
+import Personal from './personal';
 import Taro, { usePullDownRefresh } from '@tarojs/taro';
-import { RiderDeliveringTasksItemResponse, RiderGetSummaryResponse } from "../../../src/api/client";
-import './index.scss'
-import { View } from "@tarojs/components";
+import { RiderDeliveringTasksItemResponse, RiderGetSummaryResponse } from '../../../src/api/client';
+import './index.scss';
+import { View } from '@tarojs/components';
 
 const Rider: React.FC = () => {
-    const [currentPageIndex, setCurrentPageIndex] = useState(2);
-    const [deliveries, setDeliveries] = useState<RiderDeliveringTasksItemResponse[]>([])
-    const [summary, setSummary] = useState<RiderGetSummaryResponse>(new RiderGetSummaryResponse())
-    const handleTabbarClick = async (index) => {
-        if (index == 1) {
-            await handleScan()
-        } else {
-            setCurrentPageIndex(index)
-        }
+  const [currentPageIndex, setCurrentPageIndex] = useState(2);
+  const [deliveries, setDeliveries] = useState<RiderDeliveringTasksItemResponse[]>([]);
+  const [summary, setSummary] = useState<RiderGetSummaryResponse>(new RiderGetSummaryResponse());
+  const handleTabbarClick = async (index) => {
+    if (index == 1) {
+      await handleScan();
+    } else {
+      setCurrentPageIndex(index);
     }
+  };
 
-    const handleScan = async () => {
-        const scanResult = await Taro.scanCode({ onlyFromCamera: true, scanType: ['qrCode'] })
-        await API.riderClient.taskOrder(scanResult.result)
-        Taro.atMessage({
-            'message': '接单成功，赶快去配送吧',
-            'type': 'info',
-        })
-        setCurrentPageIndex(0)
-        fetchTasks()
-    }
+  const handleScan = async () => {
+    const scanResult = await Taro.scanCode({ onlyFromCamera: true, scanType: ['qrCode'] });
+    await API.riderClient.taskOrder(scanResult.result);
+    Taro.atMessage({
+      message: '接单成功，赶快去配送吧',
+      type: 'info',
+    });
+    setCurrentPageIndex(0);
+    fetchTasks();
+  };
 
-    const fetchTasks = async () => {
-        const tasks = await API.riderClient.getAllDeliveringTasks()
-        setDeliveries(tasks)
-    }
+  const fetchTasks = async () => {
+    const tasks = await API.riderClient.getAllDeliveringTasks();
+    setDeliveries(tasks);
+  };
 
-    const handleComplateDelivery = async (deliveryId: string) => {
-        await API.riderClient.complateDelivery(deliveryId)
-        await fetchTasks()
-    }
+  const handleComplateDelivery = async (deliveryId: string) => {
+    await API.riderClient.complateDelivery(deliveryId);
+    await fetchTasks();
+  };
 
-    const fetchSummary = async () => {
-        setSummary(await API.riderClient.getSummary())
-    }
+  const fetchSummary = async () => {
+    setSummary(await API.riderClient.getSummary());
+  };
 
-    useEffect(() => {
-        fetchTasks()
-        fetchSummary()
-    }, [])
+  useEffect(() => {
+    fetchTasks();
+    fetchSummary();
+  }, []);
 
-    usePullDownRefresh(async () => {
-        await fetchTasks();
-        setTimeout(() => {
-            Taro.atMessage({
-                'message': '刷新成功',
-                'type': 'info',
-                duration: 500
-            })
-            Taro.stopPullDownRefresh()
-        }, 500)
-    })
-    return (
-        <>
-            <AtMessage />
-            <View style={{ height: '100%', display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
-                <View style={{ flex: 1 }}>
-                    {currentPageIndex == 0 &&
-                        <Deliveries deliveries={deliveries} onComplateDelivery={handleComplateDelivery} />
-                    }
-                    {currentPageIndex == 2 &&
-                        <Personal summary={summary} />
-                    }
-                    <View style={{ height: 120 }}></View>
-                </View>
-                <AtTabBar
-                    fixed
-                    tabList={[
-                        { title: '当前配送', iconType: 'bullet-list' },
-                        { title: '接单', iconType: 'camera' },
-                        { title: '我的', iconType: 'folder' }
-                    ]}
-                    onClick={(index) => handleTabbarClick(index)}
-                    current={currentPageIndex}
-                />
-            </View>
-        </>
-    )
-}
+  usePullDownRefresh(async () => {
+    await fetchTasks();
+    setTimeout(() => {
+      Taro.atMessage({
+        message: '刷新成功',
+        type: 'info',
+        duration: 500,
+      });
+      Taro.stopPullDownRefresh();
+    }, 500);
+  });
+  return (
+    <>
+      <AtMessage />
+      <View style={{ height: '100%', display: 'flex', flexDirection: 'column', overflow: 'auto' }}>
+        <View style={{ flex: 1 }}>
+          {currentPageIndex == 0 && <Deliveries deliveries={deliveries} onComplateDelivery={handleComplateDelivery} />}
+          {currentPageIndex == 2 && <Personal summary={summary} />}
+          <View style={{ height: 120 }}></View>
+        </View>
+        <AtTabBar
+          fixed
+          tabList={[
+            { title: '当前配送', iconType: 'bullet-list' },
+            { title: '接单', iconType: 'camera' },
+            { title: '我的', iconType: 'folder' },
+          ]}
+          onClick={(index) => handleTabbarClick(index)}
+          current={currentPageIndex}
+        />
+      </View>
+    </>
+  );
+};
 
 export default Rider;
--- src/pages/rider/personal/index.tsx
+++ src/pages/rider/personal/index.tsx
@@ -1,83 +1,98 @@
-import { Image, Navigator, View } from "@tarojs/components";
-import React from "react";
-import { useSelector } from "react-redux";
-import { RiderGetSummaryResponse } from "src/api/client";
-import { ConnectState } from "src/models/connect";
-import { UserModelState } from "src/models/user";
-import './index.scss'
-import defaultUserAvator from '/src/static/images/my/user.png'
-import lv1Svg from '/src/static/images/my/rider/level-1.svg'
-import riderSvg from '/src/static/images/my/rider/rider.svg'
-import settlementSvg from '/src/static/images/my/rider/settlement.svg'
-import NumberMotion from "./components/NumberMotion";
-import { AtIcon } from "taro-ui";
+import { Image, Navigator, View } from '@tarojs/components';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RiderGetSummaryResponse } from 'src/api/client';
+import { ConnectState } from 'src/models/connect';
+import { UserModelState } from 'src/models/user';
+import './index.scss';
+import defaultUserAvator from '/src/static/images/my/user.png';
+import lv1Svg from '/src/static/images/my/rider/level-1.svg';
+import riderSvg from '/src/static/images/my/rider/rider.svg';
+import settlementSvg from '/src/static/images/my/rider/settlement.svg';
+import riderBg from '/src/static/images/my/rider/riderBg.png';
+import NumberMotion from './components/NumberMotion';
+import { AtIcon } from 'taro-ui';
 
 export type PersonalProps = {
-    summary: RiderGetSummaryResponse
+  summary: RiderGetSummaryResponse;
 };
 
 const renderListItem = (title: string, path: string, desc: string, showArrow: boolean) => {
-    return (<Navigator url={path} style={{ padding: '20px 20px' }}>
-        <View style={{ display: 'flex', alignItems: 'center' }}>
-            <View style={{ fontSize: 16, color: '#343434' }}>{title}</View>
-            <View style={{ flex: 1, textAlign: 'right', fontSize: 14, color: '#C1C3C6' }}>{desc}</View>
-            <View>
-                {showArrow &&
-                    <AtIcon value='chevron-right' size={14} color='#C1C3C6' customStyle={{ marginLeft: 10, verticalAlign: 'unset' }}></AtIcon>
-                }
-            </View>
-        </View>
-    </Navigator>)
-}
+  return (
+    <Navigator url={path} style={{ padding: '20px 20px' }}>
+      <View style={{ display: 'flex', alignItems: 'center' }}>
+        <View style={{ fontSize: 16, color: '#343434' }}>{title}</View>
+        <View style={{ flex: 1, textAlign: 'right', fontSize: 14, color: '#C1C3C6' }}>{desc}</View>
+        <View>{showArrow && <AtIcon value='chevron-right' size={14} color='#C1C3C6' customStyle={{ marginLeft: 10, verticalAlign: 'unset' }}></AtIcon>}</View>
+      </View>
+    </Navigator>
+  );
+};
 const Personal: React.FC<PersonalProps> = (props) => {
-    const { currentUser, isLogin } = useSelector<ConnectState, UserModelState>(x => x.user)
-    return (
-        <View className="main">
-            <View className="rider-info-card">
-                <Image className='avatar' src={currentUser ? currentUser!.avator! : defaultUserAvator} ></Image>
-                <View className="base-info">
-                    <View className="nickname">Hi,汤姆可</View>
-                    <View className="viptips">今天是你在戴夫小卖部的第67天</View>
-                </View>
-                <Image className='level' src={lv1Svg} ></Image>
-            </View>
-            <View className="account-info">
-                <View>
-                    <View> ￥<NumberMotion number={98} /> </View>
-                    <View> 今日收入 </View>
-                </View>
-                <View>
-                    <View> ￥<NumberMotion number={8487} /> </View>
-                    <View>  钱包 </View>
-                </View>
-                <View>
-                    <View> <NumberMotion number={0} /> </View>
-                    <View>  今日配送 </View>
-                </View>
-                <View>
-                    <View> <NumberMotion number={987} /> </View>
-                    <View>  累计配送 </View>
-                </View>
-            </View>
-            <View className="service-card">
-                <View className="title">骑士服务</View>
-                <View className="services">
-                    <View>
-                        <Image src={settlementSvg} ></Image>
-                        <View>  结算记录 </View>
-                    </View>
-                    <View>
-                        <Image src={riderSvg} ></Image>
-                        <View>  配送历史 </View>
-                    </View>
-                </View>
-                {/* {renderListItem('结算记录', '/pages/rider/index', '积分换购', true)}
-                {renderListItem('配送历史', '', '7*24在线客服', true)}
-                {renderListItem('技术支持', '', 'LIMO科技', true)}
-                {renderListItem('关于我们', '', ' ', true)} */}
-            </View>
+  const { currentUser, isLogin } = useSelector<ConnectState, UserModelState>((x) => x.user);
+  return (
+    <View className='main'>
+      <View className='bg'>
+        <View className='mask'></View>
+        <Image style={{ width: '100%' }} src={riderBg} mode='widthFix'></Image>
+      </View>
+      <View className='content'>
+        <View className='rider-info-card'>
+          <Image className='avatar' src={currentUser ? currentUser!.avator! : defaultUserAvator}></Image>
+          <View className='base-info'>
+            <View className='nickname'>Hi,汤姆可</View>
+            <View className='viptips'>今天是你在戴夫小卖部的第67天</View>
+          </View>
+          <Image className='level' src={lv1Svg}></Image>
         </View>
-    )
-}
+        <View className='account-info'>
+          <View>
+            <View>
+              ￥<NumberMotion number={98} />
+            </View>
+            <View> 今日收入 </View>
+          </View>
+          <View>
+            <View>
+              ￥<NumberMotion number={8487} />
+            </View>
+            <View> 钱包 </View>
+          </View>
+          <View>
+            <View>
+              <NumberMotion number={0} />
+            </View>
+            <View> 今日配送 </View>
+          </View>
+          <View>
+            <View>
+              <NumberMotion number={987} />
+            </View>
+            <View> 累计配送 </View>
+          </View>
+        </View>
+        {/* <View className='service-card'>
+        <View className='title'>骑士服务</View>
+        <View className='services'>
+          <View>
+            <Image src={settlementSvg}></Image>
+            <View> 结算记录 </View>
+          </View>
+          <View>
+            <Image src={riderSvg}></Image>
+            <View> 配送历史 </View>
+          </View>
+        </View>
+      </View> */}
+        <View style={{ backgroundColor: '#fff', borderRadius: 5 }}>
+          {renderListItem('结算记录', '/pages/rider/index', '满30即可结算', true)}
+          {renderListItem('配送历史', '', '争议订单(1)', true)}
+          {renderListItem('技术支持', '', 'LIMO科技', true)}
+          {renderListItem('关于我们', '', ' ', true)}
+        </View>
+      </View>
+    </View>
+  );
+};
 
 export default Personal;

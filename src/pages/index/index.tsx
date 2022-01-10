1e3fb3fbb81fa22c7f26c4b106384cf71d7abac5
Liu Ke <liuke0821@163.com>
2022-01-10 12:57:17 +0800

    feat menu

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -27,10 +27,10 @@ const Index: React.FC = () => {
   const [store, setStore] = useState<BaseInfoVoOfStoreInfoResponse>(new BaseInfoVoOfStoreInfoResponse());
   const [categories, setCategories] = useState<Array<CategoryInfoVoOfStoreInfoResponse>>([]);
 
-  const [productPosition, setProductPosition] = useState<Array<{ id: string; top: number; bottom: number }>>([]);
-  const [currentCategoryId, setCurrentCategoryId] = useState<string>('');
-  const [productsScrollTop, setProductsScrollTop] = useState<number>(0);
-  const [scrollIntoView, setScrollIntoView] = useState<string>('');
+  const [productPosition, setProductPosition] = useState<Array<{ id: string; top: number; bottom: number; name: string }>>([]);
+  const [menuCurrentCategoryId, setMenuCurrentCategoryId] = useState<string>('');
+  const [productCurrentCategoryId, setProductCurrentCategoryId] = useState<string>('');
+  const [scrollMutualFlag, setScrollMutualFlag] = useState<boolean>(false);
 
   const [cart, setCart] = useState<CardItem[]>([]);
   const [productModal, setProductModal] = useState<{ visible: boolean; product: ProductOfCategoryInfoVoOfStoreInfoResponse | null }>({
@@ -52,7 +52,7 @@ const Index: React.FC = () => {
       const data = await API.storeClient.getStoreById('B1FD5A56-93A4-59A4-33E0-3A01161DD34D');
       setCategories(data.categoryInfo!);
       setStore(data.baseInfo!);
-      setCurrentCategoryId(data.categoryInfo![0].id);
+      setMenuCurrentCategoryId(data.categoryInfo![0].id);
     };
     fetchCategories();
   }, []);
@@ -72,7 +72,6 @@ const Index: React.FC = () => {
         },
         (data) => {
           reslove(data.height);
-          console.log(data.height);
         }
       ).exec();
     });
@@ -82,7 +81,7 @@ const Index: React.FC = () => {
     var h = 0;
     const positions = await Promise.all(
       categories.map(async (x) => {
-        var position = { id: x.id, top: 0, bottom: 0 };
+        var position = { id: x.id, top: 0, bottom: 0, name: x.name! };
         var domHeght = await getDomHeight(`#category-${x.id}`);
         position.top = h;
         h += Math.floor(domHeght);
@@ -94,19 +93,24 @@ const Index: React.FC = () => {
   };
 
   const handleMenuSelected = (id: string) => {
-    setCurrentCategoryId(id);
-    // setProductsScrollTop(productPosition.find((item) => item.id == id)!.top);
+    // menu 点击时 同时设置Product和Menu的id
+    setProductCurrentCategoryId(id);
+    setMenuCurrentCategoryId(id);
+
+    // ProductCurrentCategoryId更新带来的自动滚动会触发productsScroll，设此参数意义为 1500ms 内不执行滚动带来的事件处理
+    setScrollMutualFlag(true);
+    setTimeout(() => setScrollMutualFlag(false), 1500);
   };
 
   const productsScroll = ({ detail }) => {
-    console.log(111);
+    // 由menu触发的滚动不做处理
+    if (scrollMutualFlag) {
+      return;
+    }
     const { scrollTop } = detail;
     let tabs = productPosition.filter((item) => item.top <= scrollTop).reverse();
-    console.log(tabs);
     if (tabs.length > 0) {
-      setTimeout(() => {
-        setCurrentCategoryId(tabs[0].id);
-      }, 2000);
+      setMenuCurrentCategoryId(tabs[0].id);
     }
   };
 
@@ -153,79 +157,73 @@ const Index: React.FC = () => {
   };
 
   return (
-    <>
-      {/* 适配加载中 */}
-      {categories.length > 0 && (
-        <View className='container'>
-          <View className='store'>
-            <View className='title'>
-              <View style={{ fontSize: 17 }}>{store.name}</View>
-              <View style={{ marginTop: 5, fontSize: 13, color: '#999' }}> 距离您{location && haversine([location.latitude, location.longitude], [store.latitude, store.longitude]).toFixed(0)}m</View>
-            </View>
-          </View>
-          <View className='main'>
-            <ScrollView className='menu-bar' enableFlex scrollY scrollWithAnimation scrollIntoView={`category-${currentCategoryId}`}>
-              <View className='wrapper'>
-                {categories.map((category) => {
-                  return (
-                    <View className={`menu-item ${currentCategoryId == category.id ? 'active' : ''}`} onClick={() => handleMenuSelected(category.id)} id={`category-${category.id}`} key={category.id}>
-                      {/* <image :src="category.category_image_url" className="image" mode="widthFix"></image> */}
-                      <View className='title'>{category.name}</View>
-                    </View>
-                  );
-                })}
-              </View>
-            </ScrollView>
-            <ScrollView className='product-section' scrollY enableFlex scrollWithAnimation scrollIntoView={`category-${currentCategoryId}`} scrollTop={productsScrollTop} onScroll={productsScroll}>
-              {categories.map((category) => {
-                return (
-                  <View className='category' key={category.id} id={`category-${category.id}`}>
-                    <View className='category.name'>{category.name}</View>
-                    <View className='products'>
-                      {category.products!.map((product) => {
-                        return (
-                          <View className='product' key={product.id}>
-                            {
-                              // 只渲染当前分类和相邻分类
-                              Math.abs(categories.findIndex((x) => x.id === category.id) - categories.findIndex((x) => x.id === currentCategoryId)) <= 1 && (
-                                <>
-                                  <Image mode='aspectFill' src={product.imgUrl!} className='image'></Image>
-                                  <View className='content'>
-                                    <View className='name'>{product.name}</View>
-                                    <View className='labels'>
-                                      {product.attributes!.map((attribute) => {
-                                        return (
-                                          <View className='label' key={attribute.id}>
-                                            {attribute.name}
-                                          </View>
-                                        );
-                                      })}
-                                    </View>
-                                    <View className='description'>{product.description || ''}</View>
-                                    <View className='price'>
-                                      <View>￥{product.price}</View>
-                                      <Action isMultiSku={product.isMultiSku} number={productCartNum(product.id)} onAdd={() => handleAddToCart({ productId: product.id, skuId: product.skus![0].id, productName: product.name!, skuPrice: product.skus![0].price, number: 1, image: product.imgUrl! })} onMinus={() => handleMinusFromCart(product.skus![0].id)} onSelectMaterails={() => setProductModal({ visible: true, product })}></Action>
-                                    </View>
-                                  </View>
-                                </>
-                              )
-                            }
-                          </View>
-                        );
-                      })}
-                    </View>
-                  </View>
-                );
-              })}
-            </ScrollView>
-          </View>
-          <View>
-            {cart.length > 0 && <CartBar cart={cart} onClear={() => {}} onAdd={(item) => handleAddToCart(item)} onMinus={(skuId) => handleMinusFromCart(skuId)} onDetail={() => {}} onPay={() => toPaymentPage()}></CartBar>}
-            {productModal.visible && <ProductModal onAddToCart={(cartItem) => handleAddToCart(cartItem)} product={productModal.product!} onClose={() => setProductModal({ ...productModal, visible: false })}></ProductModal>}
-          </View>
+    <View className='container'>
+      <View className='store'>
+        <View className='title'>
+          <View style={{ fontSize: 17 }}>{store.name}</View>
+          <View style={{ marginTop: 5, fontSize: 13, color: '#999' }}> 距离您{location && haversine([location.latitude, location.longitude], [store.latitude, store.longitude]).toFixed(0)}m</View>
         </View>
-      )}
-    </>
+      </View>
+      <View className='main'>
+        <ScrollView className='menu-bar' enableFlex scrollY scrollWithAnimation scrollIntoView={`menu-${menuCurrentCategoryId}`}>
+          <View className='wrapper'>
+            {categories.map((category) => {
+              return (
+                <View className={`menu-item ${menuCurrentCategoryId == category.id ? 'active' : ''}`} onClick={() => handleMenuSelected(category.id)} id={`menu-${category.id}`} key={category.id}>
+                  <View className='title'>{category.name}</View>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+        <ScrollView className='product-section' scrollY enableFlex scrollIntoView={`category-${productCurrentCategoryId}`} scrollWithAnimation={productCurrentCategoryId ? false : true} onScroll={productsScroll}>
+          {categories.map((category) => {
+            return (
+              <View style={{ padding: '0 10px', paddingBottom: 65 }} className='category' key={category.id} id={`category-${category.id}`}>
+                <View className='category.name'>{category.name}</View>
+                <View className='products'>
+                  {category.products!.map((product) => {
+                    return (
+                      <View className='product' key={product.id}>
+                        {
+                          // 只渲染当前分类和相邻分类
+                          Math.abs(categories.findIndex((x) => x.id === category.id) - categories.findIndex((x) => x.id === menuCurrentCategoryId)) <= 1 && (
+                            <>
+                              <Image mode='aspectFill' src={product.imgUrl!} className='image'></Image>
+                              <View className='content'>
+                                <View className='name'>{product.name}</View>
+                                <View className='labels'>
+                                  {product.attributes!.map((attribute) => {
+                                    return (
+                                      <View className='label' key={attribute.id}>
+                                        {attribute.name}
+                                      </View>
+                                    );
+                                  })}
+                                </View>
+                                <View className='description'>{product.description || ''}</View>
+                                <View className='price'>
+                                  <View>￥{product.price}</View>
+                                  <Action isMultiSku={product.isMultiSku} number={productCartNum(product.id)} onAdd={() => handleAddToCart({ productId: product.id, skuId: product.skus![0].id, productName: product.name!, skuPrice: product.skus![0].price, number: 1, image: product.imgUrl! })} onMinus={() => handleMinusFromCart(product.skus![0].id)} onSelectMaterails={() => setProductModal({ visible: true, product })}></Action>
+                                </View>
+                              </View>
+                            </>
+                          )
+                        }
+                      </View>
+                    );
+                  })}
+                </View>
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+      <View>
+        {cart.length > 0 && <CartBar cart={cart} onClear={() => {}} onAdd={(item) => handleAddToCart(item)} onMinus={(skuId) => handleMinusFromCart(skuId)} onDetail={() => {}} onPay={() => toPaymentPage()}></CartBar>}
+        {productModal.visible && <ProductModal onAddToCart={(cartItem) => handleAddToCart(cartItem)} product={productModal.product!} onClose={() => setProductModal({ ...productModal, visible: false })}></ProductModal>}
+      </View>
+    </View>
   );
 };
 

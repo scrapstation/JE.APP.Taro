6c1142b18983b3bed29216b79193b0a5ba3dc389
Liu Ke <liuke0821@163.com>
2022-03-09 18:13:17 +0800

    feat: update coupon

--- src/pages/coupon/index.tsx
+++ src/pages/coupon/index.tsx
@@ -2,17 +2,17 @@ import { Text, View } from '@tarojs/components';
 import { AtButton } from 'taro-ui';
 import './index.scss';
 
-const renderCouponItem = () => {
+const renderCouponItem = (num: number, unit: string, limitText: string) => {
   return (
-    <View className='left' style={{ backgroundColor: '#FFF', display: 'flex', margin: 20, padding: 15, borderRadius: 8, alignItems: 'center' }}>
+    <View className='left' style={{ backgroundColor: '#FFF', display: 'flex', margin: 15, padding: '12px 15px', borderRadius: 8, alignItems: 'center' }}>
       <View style={{ width: 70, marginRight: 10, color: '#daa871', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <View>
-          <Text className='neutra-font' style={{ fontSize: 32 }}>
-            9
+          <Text className='neutra-font' style={{ fontSize: num < 100 ? 32 : 28 }}>
+            {num}
           </Text>
-          <Text style={{ fontSize: 13, fontWeight: 500 }}>折</Text>
+          <Text style={{ fontSize: 13, fontWeight: 500 }}>{unit}</Text>
         </View>
-        <View style={{ fontSize: 11, color: '#dda25e' }}>无门槛</View>
+        <View style={{ fontSize: 11, color: '#dda25e' }}>{limitText}</View>
       </View>
       <View style={{ flex: 1, margin: '0 10px' }}>
         <View className='info-name'>限定饮品9折券</View>
@@ -27,5 +27,9 @@ const renderCouponItem = () => {
   );
 };
 export default () => {
-  return [1, 2, 3].map((x) => renderCouponItem());
+  return [
+    { num: 1, unit: '元', limitText: '无门槛' },
+    { num: 200, unit: '元', limitText: '满199可用' },
+    { num: 9, unit: '折', limitText: '最多折扣30元' },
+  ].map((x) => renderCouponItem(x.num, x.unit, x.limitText));
 };
